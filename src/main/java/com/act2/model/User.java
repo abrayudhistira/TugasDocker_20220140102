7cb1ded280690637a01aaf7b91f4b5083e7232d9
abrayudhistira <abrayudhistira59@gmail.com>
2026-04-13 09:57:31 +0700

    User Model: Type Data Corrected and Creating Constructor

--- src/main/java/com/act2/model/User.java
+++ src/main/java/com/act2/model/User.java
@@ -4,6 +4,13 @@ public class User {
     private String id;
     private String nama;
     private String nim;
-    private Gender;
+    private String gender;
+
+    public User(String id, String nama, String nim, String gender ){
+        this.id = id;
+        this.nama = nama;
+        this.nim = nim;
+        this.gender = gender;
+    }
 
 }
