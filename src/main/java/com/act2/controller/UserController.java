a700630fac239495238b2ed4f960ec7913d102e1
abrayudhistira <abrayudhistira59@gmail.com>
2026-04-13 10:05:31 +0700

    controller: Login Controller updated

--- src/main/java/com/act2/controller/UserController.java
+++ src/main/java/com/act2/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class UserController {
 
@@ -21,9 +23,11 @@ public class UserController {
     public String login(
         @RequestParam String username,
         @RequestParam String password,
+        HttpSession session,
             Model model) {
         if (username.equals(USERNAME) && password.equals(PASSWORD))
             {
+                session.setAttribute("isLoggedIn", true);
                 return "home";
             } else {
                 model.addAttribute("error","Username atau Password kosong");
